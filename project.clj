bf4a4b81f127952bef2fb9540f9d75a057dba015
Timothy Baldridge <tbaldridge@gmail.com>
2013-07-15 14:43:20 -0600

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fafnir "1.0"
+(defproject fafnir "1.0.1"
   :description "A library for building complex transactions for datomic"
   :url "http://github.com/halgari/fafnir"
   :license {:name "Eclipse Public License"
