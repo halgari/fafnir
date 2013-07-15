263ff7a2d1f0064f283893c41a88ef5e1a87ab1a
Timothy Baldridge <tbaldridge@gmail.com>
2013-07-15 16:21:00 -0400

    version 1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fafnir "0.1.0-SNAPSHOT"
+(defproject fafnir "1.0"
   :description "A library for building complex transactions for datomic"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
